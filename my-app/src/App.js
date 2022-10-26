217c5de8b7785fb8b5b3b662b465ad15c5462db0
GayathriSS-19 <cp.gayathrisaisree@gmail.com>
2022-10-26 20:06:58 +0530

    Added Router for Navigation

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,22 @@
 import React from "react"
 import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from "./Components/Home"
+import Cart from "./Components/Cart"
+import Shop from "./Components/Shop"
+import Contact from "./Components/Contact"
+import Login from "./Components/Login"
+
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-  
+          <Route path="/" element={<Login/>}/>
+          <Route path="home" element={<Home/>}/>
+          <Route path="shop" element={<Shop/>}/>
+          <Route path="contact" element={<Contact/>}/>
+          <Route path="cart" element={<Cart/>}/>
         </Routes>
       </BrowserRouter>
     </div>
