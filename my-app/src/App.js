a7d16b78aae45a635661150439c92e67f02b7e51
GayathriSS-19 <cp.gayathrisaisree@gmail.com>
2022-10-27 10:35:12 +0530

    Added routing for payment and shipping components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,8 @@ import Cart from "./Components/Cart"
 import Shop from "./Components/Shop"
 import Contact from "./Components/Contact"
 import Login from "./Components/Login"
+import Shipping from "./Components/Shipping"
+import Payment from "./Components/Payment"
 
 
 function App() {
@@ -13,10 +15,12 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login/>}/>
-          <Route path="home" element={<Home/>}/>
+          <Route path="/home" element={<Home/>}/>
           <Route path="shop" element={<Shop/>}/>
           <Route path="contact" element={<Contact/>}/>
           <Route path="cart" element={<Cart/>}/>
+          <Route path="cart/payment" element={<Payment/>}/>
+          <Route path="cart/shipping" element={<Shipping/>}/>
         </Routes>
       </BrowserRouter>
     </div>
