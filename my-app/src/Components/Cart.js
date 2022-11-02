1171e6114ed81ea260cf931e286d776c2801f671
GayathriSS-19 <cp.gayathrisaisree@gmail.com>
2022-11-02 20:17:21 +0530

    Removed some extra elements
    To be present in Payment.js

--- my-app/src/Components/Cart.js
+++ my-app/src/Components/Cart.js
@@ -60,33 +60,6 @@ function Cart() {
         </table>
     </section>
 
-    <section id="cart-add" className="section-p1">
-        <div id="coupon">
-            <h3>Apply Coupon</h3>
-            <div>
-                <input type="text" placeholder="Enter Your Coupon"/>
-                <button className="normal">Apply</button>
-            </div>
-        </div>
-        <div id="subtotal">
-            <h3>Cart Total</h3>
-            <table>
-                <tr>
-                    <td>Cart Subtotal</td>
-                    <td>&#8377;384.47</td>
-                </tr>
-                <tr>
-                    <td>Shipping</td>
-                    <td>Free</td>
-                </tr>
-                <tr>
-                    <td><strong>Total</strong></td>
-                    <td>&#8377;384.47</td>
-                </tr>
-            </table>
-            <button className="normal">Proceed to checkout</button>
-        </div>
-    </section>
       <Footer/>
     </div>
   )
