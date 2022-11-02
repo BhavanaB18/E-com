7af68b017f1be37be75a1e6a61d4caf5df9e179e
GayathriSS-19 <cp.gayathrisaisree@gmail.com>
2022-11-02 20:15:45 +0530

    Updated NavBar

--- my-app/src/Components/NavBar.js
+++ my-app/src/Components/NavBar.js
@@ -11,7 +11,7 @@ function NavBar() {
       <Link href="#"><img src={logo} className="logo" alt=""/></Link>    
         <div>
             <ul id="navbar">
-                <li><Link className="active" to="/home">Home</Link></li>
+                <li><Link to="/home">Home</Link></li>
                 <li><Link to="/shop">Shop</Link></li>
                 <li><Link to="/contact">Contact</Link></li>
                 <li id="lg-bag"><Link to="/cart"><i className="fa fa-shopping-cart"></i></Link></li>
