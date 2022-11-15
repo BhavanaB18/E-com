af399d5df64bf0c90682e1768005af3be48a70ae
GayathriSS-19 <cp.gayathrisaisree@gmail.com>
2022-11-15 20:04:42 +0530

    Added Products data

---
+++ Backend/data/products.js
@@ -0,0 +1,130 @@
+products=[
+    {
+        name:"Formal shirts for men",
+        desc:"Formal Shirts For Men: Choose from a wide range of mens cotton formal shirts online.",
+        image:"../../front-end/my-app/src/img/products/n1.jpg",
+        regular_p:900,
+        discount_p:799,
+        Quantity:15
+    },
+    {
+        name:"Formal shirts for men",
+        desc:"Formal Shirts For Men: Choose from a wide range of mens cotton formal shirts online.",
+        image:"../../front-end/my-app/src/img/products/n2.jpg",
+        regular_p:990,
+        discount_p:800,
+        Quantity:15
+    },
+    {
+        name:"Formal shirts for men",
+        desc:"Formal Shirts For Men: Choose from a wide range of mens cotton formal shirts online.",
+        image:"../../front-end/my-app/src/img/products/n3.jpg",
+        regular_p:990,
+        discount_p:899,
+        Quantity:15
+    },
+    {
+        name:"Casual shirts for men",
+        desc:"Casual Shirts For Men: Choose from a wide range of mens cotton casual shirts online.",
+        image:"../../front-end/my-app/src/img/products/n4.jpg",
+        regular_p:1000,
+        discount_p:899,
+        Quantity:15
+    },
+    {
+        name:"Formal shirts for men",
+        desc:"Formal Shirts For Men: Choose from a wide range of mens cotton formal shirts online.",
+        image:"../../front-end/my-app/src/img/products/n5.jpg",
+        regular_p:990,
+        discount_p:850,
+        Quantity:15
+    },
+    {
+        name:"Zara shorts for men",
+        desc:"Zara Shorts For Men: Choose from a wide range of shorts online.",
+        image:"../../front-end/my-app/src/img/products/n6.jpg",
+        regular_p:1299,
+        discount_p:1149,
+        Quantity:15
+    },
+    {
+        name:"Casual shirts for men",
+        desc:"Casual Shirts For Men: Choose from a wide range of mens cotton casual shirts online.",
+        image:"../../front-end/my-app/src/img/products/n7.jpg",
+        regular_p:990,
+        discount_p:850,
+        Quantity:15
+    },
+    {
+        name:"Casual shirts for men",
+        desc:"Casual Shirts For Men: Choose from a wide range of mens cotton casual shirts online.",
+        image:"../../front-end/my-app/src/img/products/n8.jpg",
+        regular_p:990,
+        discount_p:799,
+        Quantity:15
+    },
+    {
+        name:"Floral shirts for men",
+        desc:"Floral Shirts For Men: Choose from a wide range of mens cotton floral shirts online.",
+        image:"../../front-end/my-app/src/img/products/f1.jpg",
+        regular_p:900,
+        discount_p:799,
+        Quantity:15
+    },
+    {
+        name:"Floral shirts for men",
+        desc:"Floral Shirts For Men: Choose from a wide range of mens cotton floral shirts online.",
+        image:"../../front-end/my-app/src/img/products/f2.jpg",
+        regular_p:900,
+        discount_p:799,
+        Quantity:15
+    },
+    {
+        name:"Floral shirts for men",
+        desc:"Floral Shirts For Men: Choose from a wide range of mens cotton floral shirts online.",
+        image:"../../front-end/my-app/src/img/products/f3.jpg",
+        regular_p:900,
+        discount_p:799,
+        Quantity:15
+    },
+    {
+        name:"Floral shirts for men",
+        desc:"Floral Shirts For Men: Choose from a wide range of mens cotton floral shirts online.",
+        image:"../../front-end/my-app/src/img/products/f4.jpg",
+        regular_p:900,
+        discount_p:799,
+        Quantity:15
+    },
+    {
+        name:"Floral shirts for men",
+        desc:"Floral Shirts For Men: Choose from a wide range of mens cotton floral shirts online.",
+        image:"../../front-end/my-app/src/img/products/f5.jpg",
+        regular_p:900,
+        discount_p:799,
+        Quantity:15
+    },
+    {
+        name:"Casual shirts for men",
+        desc:"Casual Shirts For Men: Choose from a wide range of mens cotton casual shirts online.",
+        image:"../../front-end/my-app/src/img/products/f6.jpg",
+        regular_p:999,
+        discount_p:899,
+        Quantity:15
+    },
+    {
+        name:"Palazzo for women",
+        desc:"Palazzo For Women: Choose from a wide range of women cotton Palazzo pants online.",
+        image:"../../front-end/my-app/src/img/products/f7.jpg",
+        regular_p:800,
+        discount_p:750,
+        Quantity:15
+    },
+    {
+        name:"Casual tops for women",
+        desc:"Casual Shirts For Women: Choose from a wide range of women cotton western tops online.",
+        image:"../../front-end/my-app/src/img/products/f8.jpg",
+        regular_p:600,
+        discount_p:499,
+        Quantity:15
+    },
+]
