686856d52c19c508f1fec1ab6571233a09a5ebaf
GayathriSS-19 <cp.gayathrisaisree@gmail.com>
2022-11-24 12:09:56 +0530

    Retrieved products using Cart contect

--- front-end/src/Components/Home.js
+++ front-end/src/Components/Home.js
@@ -7,40 +7,55 @@ import f3 from '../img/features/f3.png';
 import f4 from '../img/features/f4.png';
 import f5 from '../img/features/f5.png';
 import f6 from '../img/features/f6.png';
+import { CartState } from '../context/ProductsContext/Contexts';
 
 import axios from "axios"
 // import "./Home.css"
 import "./style.css"
 
 function Home() {
-    const [products,setProducts]=useState([])
-    const [num,setNum]=useState(0)
+    // const [products,setProducts]=useState([])
+    // const [num,setNum]=useState(0)
     
-    const myTimeout = setTimeout(mycnt, 1);
+    // const myTimeout = setTimeout(mycnt, 1);
 
-    function mycnt() {
-        if(num<1){
-        setNum(num+1);
-        }
-        else{
-            clearTimeout(myTimeout);
-        }
-    }
-     useEffect(()=>{
-        axios.get("http://localhost:5000/products").then(response=>{
-        console.log("data from response",response.data)
-        setProducts(response.data)
-        console.log("products",products)
-       })
+    // function mycnt() {
+    //     if(num<1){
+    //     setNum(num+1);
+    //     }
+    //     else{
+    //         clearTimeout(myTimeout);
+    //     }
+    // }
+    //  useEffect(()=>{
+    //     axios.get("http://localhost:5000/products").then(response=>{
+    //     console.log("data from response",response.data)
+    //     setProducts(response.data)
+    //     console.log("products",products)
+    //    })
 
-     },[num])
+    //  },[num])
      
 
+    //  const response=CartState()
+    //  console.log(products,"Before context")
+    //  setProducts(response.state)
+    //  console.log(products,"After context")
+     
+    //  if (response.state.length!==0){
+    //     setPr
+    //  }
+
+    const response=CartState()
+    // setProducts(response)
+
+    console.log("products from context",response)
   return (
     <>
-    { products.length ?
+    
     <div>
             <NavBar />
+           
             <section id="hero">
                 <h4>Trade-in-offer</h4>
                 <h2>Super value deals</h2>
@@ -81,10 +96,10 @@ function Home() {
                 <p>Summer Collection New Modern Design</p>
                 <div className="pro-container">
                     <div className="pro">
-                        <img src={products[0].image} alt=""/>
+                        <img src={response[0].image} alt=""/>
                         <div className="des">
                             <span>Adidas</span>
-                            <h5>{products[0].name}</h5>
+                            <h5>{response[0].name}</h5>
                             <div className="star">
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
@@ -92,15 +107,15 @@ function Home() {
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
                             </div>
-                            <h4>&#8377;{products[0].discount_p}</h4>
+                            <h4>&#8377;{response[0].discount_p}</h4>
                         </div>
                         <a href="/#"><i className="fa fa-shopping-cart cart"></i></a>
                     </div>
                     <div className="pro">
-                        <img src={products[1].image} alt=""/>
+                        <img src={response[1].image} alt=""/>
                         <div className="des">
                             <span>Adidas</span>
-                            <h5>{products[1].name}</h5>
+                            <h5>{response[1].name}</h5>
                             <div className="star">
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
@@ -108,15 +123,15 @@ function Home() {
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
                             </div>
-                            <h4>&#8377;{products[1].discount_p}</h4>
+                            <h4>&#8377;{response[1].discount_p}</h4>
                         </div>
                         <a href="/#"><i className="fa fa-shopping-cart cart"></i></a>
                     </div>
                     <div className="pro">
-                        <img src={products[2].image} alt=""/>
+                        <img src={response[2].image} alt=""/>
                         <div className="des">
                             <span>Adidas</span>
-                            <h5>{products[2].name}</h5>
+                            <h5>{response[2].name}</h5>
                             <div className="star">
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
@@ -124,15 +139,15 @@ function Home() {
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
                             </div>
-                            <h4>&#8377;{products[2].discount_p}</h4>
+                            <h4>&#8377;{response[2].discount_p}</h4>
                         </div>
                         <a href="/#"><i className="fa fa-shopping-cart cart"></i></a>
                     </div>
                     <div className="pro">
-                        <img src={products[3].image} alt=""/>
+                        <img src={response[3].image} alt=""/>
                         <div className="des">
                             <span>Adidas</span>
-                            <h5>{products[3].name}</h5>
+                            <h5>{response[3].name}</h5>
                             <div className="star">
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
@@ -140,15 +155,15 @@ function Home() {
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
                             </div>
-                            <h4>&#8377;{products[3].discount_p}</h4>
+                            <h4>&#8377;{response[3].discount_p}</h4>
                         </div>
                         <a href="/#"><i className="fa fa-shopping-cart cart"></i></a>
                     </div>
                     <div className="pro">
-                        <img src={products[4].image} alt=""/>
+                        <img src={response[4].image} alt=""/>
                         <div className="des">
                             <span>Adidas</span>
-                            <h5>{products[4].name}</h5>
+                            <h5>{response[4].name}</h5>
                             <div className="star">
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
@@ -156,15 +171,15 @@ function Home() {
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
                             </div>
-                            <h4>&#8377;{products[4].discount_p}</h4>
+                            <h4>&#8377;{response[4].discount_p}</h4>
                         </div>
                         <a href="/#"><i className="fa fa-shopping-cart cart"></i></a>
                     </div>
                     <div className="pro">
-                        <img src={products[5].image} alt=""/>
+                        <img src={response[5].image} alt=""/>
                         <div className="des">
                             <span>Adidas</span>
-                            <h5>{products[5].name}</h5>
+                            <h5>{response[5].name}</h5>
                             <div className="star">
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
@@ -172,15 +187,15 @@ function Home() {
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
                             </div>
-                            <h4>&#8377;{products[5].discount_p}</h4>
+                            <h4>&#8377;{response[5].discount_p}</h4>
                         </div>
                         <a href="/#"><i className="fa fa-shopping-cart cart"></i></a>
                     </div>
                     <div className="pro">
-                        <img src={products[6].image} alt=""/>
+                        <img src={response[6].image} alt=""/>
                         <div className="des">
                             <span>Adidas</span>
-                            <h5>{products[6].name}</h5>
+                            <h5>{response[6].name}</h5>
                             <div className="star">
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
@@ -188,15 +203,15 @@ function Home() {
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
                             </div>
-                            <h4>&#8377;{products[6].discount_p}</h4>
+                            <h4>&#8377;{response[6].discount_p}</h4>
                         </div>
                         <a href="/#"><i className="fa fa-shopping-cart cart"></i></a>
                     </div>
                     <div className="pro">
-                        <img src={products[7].image} alt=""/>
+                        <img src={response[7].image} alt=""/>
                         <div className="des">
                             <span>Adidas</span>
-                            <h5>{products[7].name}</h5>
+                            <h5>{response[7].name}</h5>
                             <div className="star">
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
@@ -204,7 +219,7 @@ function Home() {
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
                             </div>
-                            <h4>&#8377;{products[7].discount_p}</h4>
+                            <h4>&#8377;{response[7].discount_p}</h4>
                         </div>
                         <a href="/#"><i className="fa fa-shopping-cart cart"></i></a>
                     </div>
@@ -223,10 +238,10 @@ function Home() {
                 <p>Summer Collection New Modern Design</p>
                 <div className="pro-container">
                     <div className="pro">
-                        <img src={products[8].image} alt=""/>
+                        <img src={response[8].image} alt=""/>
                         <div className="des">
                             <span>Adidas</span>
-                            <h5>{products[8].name}</h5>
+                            <h5>{response[8].name}</h5>
                             <div className="star">
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
@@ -234,15 +249,15 @@ function Home() {
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
                             </div>
-                            <h4>&#8377;{products[8].discount_p}</h4>
+                            <h4>&#8377;{response[8].discount_p}</h4>
                         </div>
                         <a href="/#"><i className="fa fa-shopping-cart cart"></i></a>
                     </div>
                     <div className="pro">
-                        <img src={products[9].image} alt=""/>
+                        <img src={response[9].image} alt=""/>
                         <div className="des">
                             <span>Adidas</span>
-                            <h5>{products[9].name}</h5>
+                            <h5>{response[9].name}</h5>
                             <div className="star">
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
@@ -250,15 +265,15 @@ function Home() {
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
                             </div>
-                            <h4>&#8377;{products[9].discount_p}</h4>
+                            <h4>&#8377;{response[9].discount_p}</h4>
                         </div>
                         <a href="/#"><i className="fa fa-shopping-cart cart"></i></a>
                     </div>
                     <div className="pro">
-                        <img src={products[10].image} alt=""/>
+                        <img src={response[10].image} alt=""/>
                         <div className="des">
                             <span>Adidas</span>
-                            <h5>{products[10].name}</h5>
+                            <h5>{response[10].name}</h5>
                             <div className="star">
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
@@ -266,15 +281,15 @@ function Home() {
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
                             </div>
-                            <h4>&#8377;{products[10].discount_p}</h4>
+                            <h4>&#8377;{response[10].discount_p}</h4>
                         </div>
                         <a href="/#"><i className="fa fa-shopping-cart cart"></i></a>
                     </div>
                     <div className="pro">
-                        <img src={products[11].image} alt=""/>
+                        <img src={response[11].image} alt=""/>
                         <div className="des">
                             <span>Adidas</span>
-                            <h5>{products[11].name}</h5>
+                            <h5>{response[11].name}</h5>
                             <div className="star">
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
@@ -282,15 +297,15 @@ function Home() {
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
                             </div>
-                            <h4>&#8377;{products[11].discount_p}</h4>
+                            <h4>&#8377;{response[11].discount_p}</h4>
                         </div>
                         <a href="/#"><i className="fa fa-shopping-cart cart"></i></a>
                     </div>
                     <div className="pro">
-                        <img src={products[12].image} alt=""/>
+                        <img src={response[12].image} alt=""/>
                         <div className="des">
                             <span>Adidas</span>
-                            <h5>{products[12].name}</h5>
+                            <h5>{response[12].name}</h5>
                             <div className="star">
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
@@ -298,15 +313,15 @@ function Home() {
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
                             </div>
-                            <h4>&#8377;{products[12].discount_p}</h4>
+                            <h4>&#8377;{response[12].discount_p}</h4>
                         </div>
                         <a href="/#"><i className="fa fa-shopping-cart cart"></i></a>
                     </div>
                     <div className="pro">
-                        <img src={products[13].image} alt=""/>
+                        <img src={response[13].image} alt=""/>
                         <div className="des">
                             <span>Adidas</span>
-                            <h5>{products[13].name}</h5>
+                            <h5>{response[13].name}</h5>
                             <div className="star">
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
@@ -314,15 +329,15 @@ function Home() {
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
                             </div>
-                            <h4>&#8377;{products[13].discount_p}</h4>
+                            <h4>&#8377;{response[13].discount_p}</h4>
                         </div>
                         <a href="/#"><i className="fa fa-shopping-cart cart"></i></a>
                     </div>
                     <div className="pro">
-                        <img src={products[14].image} alt=""/>
+                        <img src={response[14].image} alt=""/>
                         <div className="des">
                             <span>Adidas</span>
-                            <h5>{products[14].name}</h5>
+                            <h5>{response[14].name}</h5>
                             <div className="star">
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
@@ -330,15 +345,15 @@ function Home() {
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
                             </div>
-                            <h4>&#8377;{products[14].discount_p}</h4>
+                            <h4>&#8377;{response[14].discount_p}</h4>
                         </div>
                         <a href="/#"><i className="fa fa-shopping-cart cart"></i></a>
                     </div>
                     <div className="pro">
-                        <img src={products[15].image} alt=""/>
+                        <img src={response[15].image} alt=""/>
                         <div className="des">
                             <span>Adidas</span>
-                            <h5>{products[15].name}</h5>
+                            <h5>{response[15].name}</h5>
                             <div className="star">
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
@@ -346,7 +361,7 @@ function Home() {
                                 <i className="fas fa-star"></i>
                                 <i className="fas fa-star"></i>
                             </div>
-                            <h4>&#8377;{products[15].discount_p}</h4>
+                            <h4>&#8377;{response[15].discount_p}</h4>
                         </div>
                         <a href="/#"><i className="fa fa-shopping-cart cart"></i></a>
                     </div>
@@ -398,7 +413,6 @@ function Home() {
             
         <Footer/>
         </div>
-    :console.log('sorry')}
     </>
     );
 }
