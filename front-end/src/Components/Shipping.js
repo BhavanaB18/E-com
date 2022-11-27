29abdb8ed88139af3cc0a639223fbb15eec3f918
GayathriSS-19 <cp.gayathrisaisree@gmail.com>
2022-11-27 21:14:38 +0530

    Added Shipping page
    Added Routing from shipping to Home

--- front-end/src/Components/Shipping.js
+++ front-end/src/Components/Shipping.js
@@ -1,10 +1,22 @@
 import React from 'react'
-import "./style.css"
+import "./Shipping.css"
+import con from "../img/confirmed.jpg"
+import { Link } from 'react-router-dom'
 
 function Shipping() {
   return (
     <div>
-      <p>This is Shipping Page</p>
+      <div class="container">
+        <div class="box">
+            <h1>Your order has been received</h1>
+            <div class="inner-box">
+                <img src={con}/>
+                <h3>Thank You for your purchase</h3>
+                <Link to="/home"><button>Continue Shopping</button></Link>
+
+            </div>
+        </div>
+    </div>
     </div>
   )
 }
