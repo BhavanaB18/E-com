893d926d0368e89a4063b1d8c22780221a28d86d
GayathriSS-19 <cp.gayathrisaisree@gmail.com>
2022-12-15 11:31:16 +0530

    Added cart count and changed hero image in home

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -3,8 +3,11 @@ import React from 'react'
 import "./style.css"
 import { Link } from "react-router-dom"
 import logo from "../img/logo.png"
+import { CartState } from '../context/ProductsContext/Contexts'
 
 function NavBar() {
+  const response=CartState()
+  let cart=response.state.cart
   return (
     <div id="header">
       {/* <p>This is NavBar</p> */}
@@ -15,6 +18,7 @@ function NavBar() {
                 <li><Link to="/shop">Shop</Link></li>
                 <li><Link to="/contact">Contact</Link></li>
                 <li id="lg-bag"><Link to="/cart"><i className="fa fa-shopping-cart"></i></Link></li>
+                <li id="len">{cart.length}</li>
             </ul>
         </div>
     </div>
